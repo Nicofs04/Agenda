84a7aa88852599e6eb34f793a0dc3449e40147fc
Nicof04 <nicofs.0405@gmail.com>
2025-07-15 16:47:44 +0200

    upload

--- main.c
+++ main.c
@@ -6,6 +6,11 @@
 int main(int argc, char* argv[]){
     char opcion;
 
+    if(argc != 2){
+        printf("Error, modo de uso: ./'nombre_programa' 'nombre_archivo'\n");
+        return EXIT_FAILURE;
+    }
+
     TCOLA cola;
     crearCola(&cola);
 
